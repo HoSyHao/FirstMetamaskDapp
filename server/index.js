8480e1ca613dfae9f9c07a21695341c14be11b1a
HoSyHao <bloodarmour1@gmail.com>
2025-07-22 10:31:54 +0700

    Remove allowedHeaders

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(cors({
   origin: [config.origin],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
